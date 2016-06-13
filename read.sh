1d89010b26aa8116eb1071a8ca5ef2443380c673
Travis R <travis@magnatecha.com>
2016-06-13 13:08:14 -0500

    done is a dir, not a file

--- read.sh
+++ read.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd ~/Documents/read
 cat `ls | head -n1` | grep "Link: " | sed s/'Link: '//g | pbcopy; 
-mv `ls | head -n1` ~/Documents/done;
+mv `ls | head -n1` ~/Documents/done/;
 ls | wc -l
 cd -
 exit;
