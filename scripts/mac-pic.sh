7f436072656c351a473cc8e5e2893aed540c931e
Travis Reddell <travisred@me.com>
2018-05-04 11:54:40 -0500

    progress for screenshot upload

--- scripts/mac-pic.sh
+++ scripts/mac-pic.sh
@@ -1,9 +1,9 @@
-#! /bin/bash 
+#! /bin/bash
 
 d=`date +%Y%m%d%I%M%S`
 host="magnatecha.com"
-screencapture -i $d.png; 
+screencapture -i $d.png;
 open $d.png #edit?
 read go;
-rsync $d.png $host:~/public_html/_media/; echo "http://$host/_media/$d.png" | pbcopy
+rsync --progress $d.png $host:~/public_html/_media/; echo "http://$host/_media/$d.png" | pbcopy
 mv $d.png ~/sites/_media/
