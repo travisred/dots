2103b78137908cb7900df8983efb9d7f8c7cb0ce
Travis Reddell <travisred@me.com>
2018-10-11 15:40:45 -0500

    adjust mutt shortcuts, add hash to screenshot script

--- scripts/mac-pic.sh
+++ scripts/mac-pic.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-d=`date +%Y%m%d%I%M%S`
-host="magnatecha.com"
-screencapture -i $d.png;
-open $d.png #edit?
+filename=`date +%Y%m%d%I%M%S`;
+filename+='_';
+filename+=`openssl rand -hex 16`;
+file_name+='.png';
+host="magnatecha.com";
+
+screencapture -i $filename;
+open $filename; #edit?
 read go;
-rsync --progress $d.png $host:~/public_html/_media/; echo "http://$host/_media/$d.png" | pbcopy
-mv $d.png ~/sites/_media/
-osascript -e 'display notification "The image has been uploaded to your server." with title "Image Uploaded"'
+
+rsync --progress $filename $host:~/public_html/_media/;
+echo "http://$host/_media/$filename" | pbcopy;
+
+mv $filename ~/sites/_media/;
+osascript -e 'display notification "The image has been uploaded to your server." with title "Image Uploaded"';
