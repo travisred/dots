f2ff9ac43d1438137e4599083efe82d4eb3e6341
Travis Reddell <tjreddell@gmail.com>
2020-04-20 11:34:12 -0500

    add some programs if linux

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ "$(uname)" == "Linux" ]
+then
+    echo "Installing vim, git, keepassx, dmenu"
+    sudo apt install vim git keepassx dmenu
+fi
+
 mv ~/.bash_aliases ~/.bash_aliases.orig
 mv ~/.vimrc ~/.vimrc.orig
 mv ~/.bashrc ~/.bashrc.orig
@@ -10,15 +25,16 @@ ln -s ~/dots/vimrc ~/.vimrc
 ln -s ~/dots/bashrc ~/.bashrc
 ln -s ~/dots/tmux.conf ~/.tmux.conf
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s /home/treddell/dots/colors .vim/
-ln -s /home/treddell/dots/muttrc /home/treddell/.muttrc
-ln -s /home/treddell/dots/mutt_personal /home/treddell/.mutt_personal
-ln -s /home/treddell/dots/mutt_work /home/treddell/.mutt_work
-ln -s /home/treddell/Documents/email/ahm /home/treddell/ahm
-ln -s /home/treddell/Documents/getmail /home/treddell/.getmail
+ln -s ~/dots/colors .vim/
+ln -s ~/dots/muttrc ~/.muttrc
+ln -s ~/dots/mutt_personal ~/.mutt_personal
+ln -s ~/dots/mutt_work ~/.mutt_work
+ln -s ~/Documents/email/ahm ~/ahm
+ln -s ~/Documents/getmail ~/.getmail
 
 git config --global user.email "tjreddell@gmail.com"
 git config --global user.name "Travis Reddell"
 git config --global core.editor "vim"
+
 
 exit
